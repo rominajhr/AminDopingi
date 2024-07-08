db281b1ca40de711069a89cc034d1cd7eeaedd2a
narges afzali <153275951+kotarou-katsura@users.noreply.github.com>
2024-07-08 19:45:47 +0330

    Create Game.h

---
+++ view/Game.h
@@ -0,0 +1,16 @@
+#ifndef  AMINDOPINGI_GAME_H
+#define  AMINDOPINGI_GAME_H
+
+#include <QGraphicsView>
+#include <QGraphicsScene>
+
+
+class Game : public QGraphicsView {
+
+
+public:
+    Game();
+
+};
+
+#endif
