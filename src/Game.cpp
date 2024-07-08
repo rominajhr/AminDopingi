84a56c348b38a6e4aa5dae7f6bdb81993f088b8e
narges afzali <153275951+kotarou-katsura@users.noreply.github.com>
2024-07-08 20:13:23 +0330

    add platforms to Game.cpp

--- src/Game.cpp
+++ src/Game.cpp
@@ -13,7 +13,40 @@ Game::Game() {
     scene->addItem(background);
     backgroundHills = new BackgroundHills(width(), height());
     scene->addItem(backgroundHills);
+    
+    int platformSpacing = 400;
+    int initialX = 0;
+    Platform *platform;
+    for (int k = 0; k < 4; ++k) {
+        
+        platform = new Platform(width(), height());
+        platform->setPos(initialX, height() - platform->pixmap().height());
+        platforms.push_back(platform);
+        scene->addItem(platform);
+        initialX += platform->pixmap().width() + platformSpacing;
 
+
+        //another platform with space
+        platform = new Platform(width(), height());
+        platform->setPos(initialX, height() - platform->pixmap().height());
+        platforms.push_back(platform);
+        scene->addItem(platform);
+        initialX += platform->pixmap().width();
+
+
+        //a platform right next to the last platform
+        platform = new Platform(width(), height());
+        platform->setPos(initialX, height() - platform->pixmap().height());
+        platforms.push_back(platform);
+        scene->addItem(platform);
+
+        //add another platform
+        platform = new Platform(width(), height());
+        platform->setPos(initialX, height() - platform->pixmap().height());
+        platforms.push_back(platform);
+        scene->addItem(platform);
+        initialX += platform->pixmap().width() + platformSpacing;
+    }
     
     setScene(scene);
 }
