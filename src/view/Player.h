a1eb440f7ede859bbe8a8cb9902f7c0a5fc3099c
narges afzali <153275951+kotarou-katsura@users.noreply.github.com>
2024-07-08 20:26:00 +0330

    Update Player.h

--- src/view/Player.h
+++ src/view/Player.h
@@ -2,13 +2,10 @@
 #define PLAYER_H
 
 #include <QGraphicsPixmapItem>
-#include <QObject>
-#include <QTimer>
-#include <QKeyEvent>
 #include "Position.h"
 
 class Player : public QObject, public QGraphicsPixmapItem {
-Q_OBJECT
+
 
 public:
     Player(int SceneWidth, int SceneHeight, QGraphicsItem *parent = nullptr);
